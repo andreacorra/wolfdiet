75eccaf2195780aeaaf028ce6e0524642cc67bb8
Andrea Corradini <38738063+andreacorra@users.noreply.github.com>
2018-12-17 23:17:40 +0100

    Update wolf_diet_db.sql

--- db_structure/wolf_diet_db.sql
+++ db_structure/wolf_diet_db.sql
@@ -1,1 +1,22 @@
+-- Schema: main
+-- DROP SCHEMA main;
 
+CREATE SCHEMA main
+  AUTHORIZATION postgres;
+
+GRANT ALL ON SCHEMA main TO postgres;
+GRANT ALL ON SCHEMA main TO public;
+COMMENT ON SCHEMA main
+  IS 'main schema';
+  
+  
+-- Schema: temp
+-- DROP SCHEMA temp;
+
+CREATE SCHEMA temp
+  AUTHORIZATION postgres;
+
+GRANT ALL ON SCHEMA temp TO postgres;
+GRANT ALL ON SCHEMA temp TO public;
+COMMENT ON SCHEMA temp
+  IS 'schema for temporary files';
