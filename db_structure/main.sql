b0f48fc8cf1e05234dcc40c18f43587bf6c788c5
Andrea Corradini <38738063+andreacorra@users.noreply.github.com>
2019-05-13 23:07:09 +0200

    added Table: main.diet_analysis

--- db_structure/main.sql
+++ db_structure/main.sql
@@ -1,6 +1,63 @@
 -- Table: main.diet_analysis
 -- DROP TABLE main.diet_analysis;
 
+CREATE TABLE main.diet_analysis(
+  diet_analysis_id serial NOT NULL,
+  source_id integer,
+  analytical_method_id integer,
+  identification_method_id integer,
+  derived_diet boolean,
+  temporal_scale_id integer,
+  sample_size integer,
+  surface_area integer,
+  sampling_period character varying,
+  time_series integer,
+  spatial_effort double precision,
+  temporal_effort double precision,
+  total_effort integer,
+  update_timestamp timestamp with time zone DEFAULT now(), 
+  insert_timestamp timestamp with time zone DEFAULT now(),
+  notes text,
+  CONSTRAINT diet_analysis_pkey PRIMARY KEY (diet_analysis_id),
+  CONSTRAINT diet_analysis_source_id_fk FOREIGN KEY (source_id)
+      REFERENCES lu_tables.lu_source (source_id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT diet_analysis_analytical_method_id_fk FOREIGN KEY (analytical_method_id)
+      REFERENCES lu_tables.lu_analytical_method (analytical_method_id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT diet_analysis_identification_method_id_fk FOREIGN KEY (identification_method_id)
+      REFERENCES lu_tables.lu_identification_method (identification_method_id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT diet_analysis_temporal_scale_id_fk FOREIGN KEY (temporal_scale_id)
+      REFERENCES lu_tables.lu_temporal_scale (temporal_scale_id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
+)
+  WITH (
+  OIDS=FALSE
+);
+  
+ALTER TABLE main.diet_analysis
+  OWNER TO postgres;
+GRANT ALL ON TABLE main.diet_analysis TO postgres;
+GRANT SELECT ON TABLE main.diet_analysis TO users;
+
+COMMENT ON TABLE main.diet_analysis IS 'Dimension table with information on analysis used to calculate the diet';
+  COMMENT ON COLUMN main.diet_analysis.diet_analysis_id IS 'database identifier for type of analysis used to calculate the diet';
+  COMMENT ON COLUMN main.diet_analysis.source_id IS 'database identifier for type of samples used in the analyses';
+  COMMENT ON COLUMN main.diet_analysis.analytical_method_id IS 'database identifier for the analytical method';
+  COMMENT ON COLUMN main.diet_analysis.derived_diet IS 'if TRUE, diet calculated using raw data in the article';
+  COMMENT ON COLUMN main.diet_analysis.temporal_scale_id IS 'database identifier for the temporal scale';
+  COMMENT ON COLUMN main.diet_analysis.sample_size IS 'total number of samples analysed in the study';
+  COMMENT ON COLUMN main.diet_analysis.surface_area IS 'surface of the study area (in km2)';
+  COMMENT ON COLUMN main.diet_analysis.sampling_period IS 'sampling duration as text';  -- Deprecated?
+  COMMENT ON COLUMN main.diet_analysis.time_series IS 'number of intervals (seasons or years) in the diet analysis';
+  COMMENT ON COLUMN main.diet_analysis.spatial_effort IS 'density of samples collected in the study area';
+  COMMENT ON COLUMN main.diet_analysis.temporal_effort IS 'average number of samples per interval'; -- defines sampling period
+  COMMENT ON COLUMN main.diet_analysis.total_effort IS 'number of months in which the sampling was carried out';
+  COMMENT ON COLUMN main.diet_analysis.update_timestamp IS 'date and time when the record was uploaded into the database';
+  COMMENT ON COLUMN main.diet_analysis.insert_timestamp IS 'date and time when the record was updated (last time)';
+  COMMENT ON COLUMN main.diet_analysis.notes IS 'additional information';
+
 
 -- Table: main.reference
 -- DROP TABLE main.reference;
