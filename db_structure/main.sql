4bc9ff1c84f4651e9a45f7717aa15a49952df5af
Andrea Corradini <38738063+andreacorra@users.noreply.github.com>
2020-04-13 16:52:19 +0200

    addedd code for exporting as SQLite

--- db_structure/main.sql
+++ db_structure/main.sql
@@ -349,6 +349,7 @@ CREATE TRIGGER update_timestamp
 
 
 
+
 -- VIEW OF THE PIVOT-TABLE
 CREATE OR REPLACE VIEW main.diet_item_users AS (
 select * from crosstab(
@@ -437,3 +438,23 @@ select * from crosstab(
 "semiaquatic rodentia" double precision,
 "undefined item" double precision,
 "vegetable fruit" double precision));
+
+										       
+										       
+										       -- CREATE SPATIALITE (run in terminal)
+-- CREATE FILE AND FIRST TABLE 
+ogr2ogr -f SQLite -dsco SPATIALITE=yes wolfdiet.sqlite PG:"dbname=wolfdiet user=postgres password=pw" main.diet_analysis
+-- CREATE OTHER TABLES 
+ogr2ogr -f SQLite -update wolfdiet.sqlite PG:"dbname=wolfdiet user=postgres password=pw" main.diet_item
+ogr2ogr -f SQLite -update wolfdiet.sqlite PG:"dbname=wolfdiet user=postgres password=pw" main.reference
+ogr2ogr -f SQLite -update wolfdiet.sqlite PG:"dbname=wolfdiet user=postgres password=pw" main.site
+ogr2ogr -f SQLite -update wolfdiet.sqlite PG:"dbname=wolfdiet user=postgres password=pw" main.wolfdiet
+ogr2ogr -f SQLite -update wolfdiet.sqlite PG:"dbname=wolfdiet user=postgres password=pw" lu_tables.lu_altitude_range
+ogr2ogr -f SQLite -update wolfdiet.sqlite PG:"dbname=wolfdiet user=postgres password=pw" lu_tables.lu_analytical_method
+ogr2ogr -f SQLite -update wolfdiet.sqlite PG:"dbname=wolfdiet user=postgres password=pw" lu_tables.lu_identification_method
+ogr2ogr -f SQLite -update wolfdiet.sqlite PG:"dbname=wolfdiet user=postgres password=pw" lu_tables.lu_source
+ogr2ogr -f SQLite -update wolfdiet.sqlite PG:"dbname=wolfdiet user=postgres password=pw" lu_tables.lu_temporal_scale
+ogr2ogr -f SQLite -update wolfdiet.sqlite PG:"dbname=wolfdiet user=postgres password=pw" lu_tables.lu_type_study
+ogr2ogr -f SQLite -update wolfdiet.sqlite PG:"dbname=wolfdiet user=postgres password=pw" lu_tables.lu_wolf_population
+ogr2ogr -f SQLite -update wolfdiet.sqlite PG:"dbname=wolfdiet user=postgres password=pw" lu_tables.lu_wolf_subspecies
+ogr2ogr -f SQLite -update wolfdiet.sqlite PG:"dbname=wolfdiet user=postgres password=pw" env_data.world_country
